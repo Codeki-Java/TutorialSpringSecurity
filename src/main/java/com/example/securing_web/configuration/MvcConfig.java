aff9520ead1adb6630da0895bdb33cfe9e465609
alejandrardecajal <alejandrardecajal@gmail.com>
2024-06-06 16:33:49 -0300

    first commit

---
+++ src/main/java/com/example/securing_web/configuration/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.example.securing_web.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//Configuracion SIN seguridad
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
